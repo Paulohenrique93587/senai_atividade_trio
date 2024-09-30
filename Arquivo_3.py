8fe3132e65c5d589c8b77069c6b937e3deabd3a8
Vinniciusjesus19 <160077268+Vinniciusjesus19@users.noreply.github.com>
2024-09-30 14:25:23 +0000

    Arquivo3

--- Arquivo_3.py
+++ Arquivo_3.py
@@ -1,1 +1,14 @@
-print("Vinnicius.")
+import os 
+os.system
+
+class Engenheiro:
+    def __init__(self, nome, crea):
+        self. nome = nome
+        self. crea = crea 
+
+        def exibir_detalhes(self):
+            print(f"Engenheiro: {self.nome}")
+            print(f"CREA: {self.crea}")
+
+engenheiro1 = Engenheiro("João Silva", "123456-7")
+engenheiro1.exibir_detalhes()
